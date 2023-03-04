e24e6211084361fa4bf3ee2bf133e5c9a3087413
tianzhu <wangtianzhu01@huohua.cn>
2023-03-04 22:36:31 +0800

    add api

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,6 @@ const fetchBlog = require('../crawler')
 
 const router = express.Router()
 
-// router.get('', () => {})
-
 router.post('/fetchblog', async (req, res) => {
     const { url } = req.body
 
@@ -31,17 +29,62 @@ router.post('/fetchblog', async (req, res) => {
     })
 });
 
+const dbQuery = async (tableName, params) => {
+    const { keywords, page, size } = params
+
+    let sql;
+    if (keywords) {
+        sql = `select * from ${tableName} where keywords regexp '${ keywords }'`
+    } else {
+        sql = `select * from ${tableName}`
+    }
+    return await MYSQL.query(sql)
+}
+
 router.post('/collectionblogs', async (req, res) => {
-    const { config } = req.body
     console.log('/api/collectionblogs was required')
-    
-    const blogs = await MYSQL.select('collection', config)
+
+    const blogs = await dbQuery('collection', req.body)
+
     res.send({
         data: blogs,
         code: 0,
         msg: 'inquire success'
     })
 });
+
+router.post('/originalblogs', async (req, res) => {
+    console.log('/api/originalblogs was required')
+    const { keywords, page, size } = req.body
+
+    let sql;
+    if (keywords) {
+        sql = `select title,description,type,source,blogid,coverSrc,keywords from article where keywords regexp '${ keywords }'`
+    } else {
+        sql = `select title,description,type,source,blogid,coverSrc,keywords from article`
+    }
+
+    const blogs = await MYSQL.query(sql)
+
+    res.send({
+        data: blogs,
+        code: 0,
+        msg: 'inquire success'
+    })
+});
+
+router.post('/originalblog', async (req, res) => {
+    console.log('/api/originalblog was required')
+    const { blogid } = req.body
+ 
+    const blog = await MYSQL.query(`select * from article where blogid = '${blogid}'`)
+
+    res.send({
+        data: blog,
+        code: 0,
+        msg: 'inquire success'
+    })
+})
 
 module.exports = router
 
