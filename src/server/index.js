75a0b5050923a7bd8d2a101cf5e823740806ab7b
tianzhu <wangtianzhu01@huohua.cn>
2023-03-07 14:04:09 +0800

    add https ssl

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const express = require('express');
 
 const history = require('connect-history-api-fallback');
@@ -6,7 +7,14 @@ var bodyParser = require('body-parser')
 const router = require('../router')
 
 async function initServer () {
+    var options = {
+        pfx: fs.readFileSync(path.join(__dirname, '9425468_blog.wangtz.cn.pfx')),
+        passphrase: 'g3a2h38t'
+    }
+
     const app = express();
+
+    const server = https.createServer(options,app);
 
     app.all("*", function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
@@ -22,7 +30,7 @@ async function initServer () {
     app.use(bodyParser.json());
     app.use('/api', router);
 
-    app.listen('8089', () => {
+    server.listen('8089', () => {
         console.log('server is running listen port 8089')
     });
 }
