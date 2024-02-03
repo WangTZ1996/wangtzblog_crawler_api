62bbfd9d97bb55ecc436afa69df1735fe7dbc7ee
WangTZ1996 <wangtianzhu01@huohua.cn>
2024-02-03 22:32:11 +0800

    add goerli_faucet

--- src/router/index.js
+++ src/router/index.js
@@ -159,4 +159,113 @@ router.post("/originalblogFormChain", async (req, res) => {
   }
 });
 
+router.post("/goerli_faucet", async (req, res) => {
+  const { address } = req.body;
+
+  console.log('request address', address)
+
+  var web3 = new Web3("https://eth-goerli.public.blastapi.io");
+  var fromAccount = "0xAb27c2e524F69b20C27Ec9A00387ec1D69142C90"
+  var privateKey = "f1e04702f122265dde30072eb972d29b5e5809cccdebe4275ec59a7e20b31699";
+  var contractAbi = [
+      {
+          "inputs": [
+              {
+                  "internalType": "address payable",
+                  "name": "_to",
+                  "type": "address"
+              }
+          ],
+          "name": "callETH",
+          "outputs": [],
+          "stateMutability": "payable",
+          "type": "function"
+      },
+      {
+          "inputs": [],
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+      },
+      {
+          "stateMutability": "payable",
+          "type": "receive"
+      },
+      {
+          "inputs": [],
+          "name": "getAddress",
+          "outputs": [
+              {
+                  "internalType": "address",
+                  "name": "",
+                  "type": "address"
+              }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "inputs": [],
+          "name": "getBalance",
+          "outputs": [
+              {
+                  "internalType": "uint256",
+                  "name": "",
+                  "type": "uint256"
+              }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "inputs": [],
+          "name": "owner",
+          "outputs": [
+              {
+                  "internalType": "address payable",
+                  "name": "",
+                  "type": "address"
+              }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+      }
+  ];
+  var contractAddress = "0xe7E97f6c9D0450ac88182A62E7b061BbD034c46E"
+  var faucet = new web3.eth.Contract(contractAbi, contractAddress);
+
+  async function getNonce() {
+    const nonce = await new Promise((res, rej) => {
+        web3.eth.getTransactionCount(fromAccount).then((txCount) => {
+            res(web3.utils.toHex(txCount))
+        })
+    })
+    return nonce
+  }
+
+  async function getGasPrice() {
+    const gasPrice = await web3.eth.getGasPrice()
+    return gasPrice
+  }
+
+  var functionEncode = await faucet.methods.callETH(address).encodeABI();
+  var nonce = await getNonce()
+  var gasPrice = await getGasPrice()
+
+  var sign = await web3.eth.accounts.signTransaction({
+      nonce,
+      gas: 300000,
+      gasPrice: web3.utils.toHex(gasPrice),
+      to: contractAddress,
+      data: functionEncode,
+  }, privateKey);
+
+  var result = await web3.eth.sendSignedTransaction(sign.rawTransaction);
+  console.log("callETH txHash = " + result.transactionHash);
+  res.send({
+    data: result.transactionHash,
+    code: 0,
+    msg: "success",
+  })
+})
+
 module.exports = router;
